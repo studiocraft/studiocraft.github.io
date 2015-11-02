af88b1e359a26f0d762b53142ca7b3b63ef6fa3b
Bryan Colosky <bcolosky@gmail.com>
2015-11-02 13:12:40 -0500

    navbar and gradients

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -19,42 +19,93 @@
     'common': {
       init: function() {
         // JavaScript to be fired on all pages
+        new WOW().init();
+
         $(document).ready(function(){
           var imgCycle = ['rndm1','rndm2'];
           var randomBg = Math.floor(Math.random() * imgCycle.length);
           var backgroundImg = imgCycle[randomBg];
           $('.bg').addClass(backgroundImg);
         });
+
         $(window).on('load resize', function (e) {
           e.preventDefault();
           var containerHeight = $('.banner').height();
           $('.banner').children().css({"min-height": containerHeight + "px"});
+
         });
+
       },
       finalize: function() {
         // JavaScript to be fired on all pages, after page specific JS is fired
-        $("a[data-scroll='animate']").click(function() {
+        var scrollTimeout;
+
+        $('a[data-scroll="smooth"]').on('click touchend', function() {
           if (location.pathname.replace(/^\//,'') === this.pathname.replace(/^\//,'') && location.hostname === this.hostname) {
             var target = $(this.hash);
             target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
             if (target.length) {
-              $('html, body').animate({
+              $('html,body').animate({
                 scrollTop: target.offset().top
               }, 1000);
               return false;
             }
           }
         });
-        $(window).scroll(function() {
-          var opacity = $(window).scrollTop() / 1000;
-          var opacityMax = 1 - 0.65;
-          if( opacity < opacityMax ) {
-            $('.overlay').css({"opacity": 0.65 + opacity});
+
+        $(window).on('scroll touchend', function () {
+            if (scrollTimeout) {
+                clearTimeout(scrollTimeout);
+                scrollTimeout = null;
+            }
+            scrollTimeout = setTimeout(scrollHandler, 20);
+        });
+        scrollHandler = function () {
+          var opacityInit = 0.65;
+          var opacityCalc = $(window).scrollTop() / 1000;
+          var opacity = opacityInit + opacityCalc;
+          var opacityMax = 1;
+
+          if(opacity < opacityMax ) {
+            $('.overlay').css({"opacity": opacity});
           } else {
-            $('.overlay').css({"opacity": 0.65 + opacityMax});
+            $('.overlay').css({"opacity": opacityMax});
           }
+        };
+      }
+    },
+    // Home page
+    'home': {
+      init: function() {
+        // JavaScript to be fired on the home page
+        $("#contactForm").submit( function(e) {
+          e.preventDefault();
+          $.ajax({
+            url: "//formspree.io/hello@studiocraft.cc",
+            method: "POST",
+            data: $(this).serialize(),
+            dataType: "json",
+            beforeSend: function() {
+              $('#formSubmit').text('Sending ... ').prop('disabled', true);
+            },
+            success: function(){
+              $('#formSubmit').text('hello@studiocraft.cc').prop('disabled', false);
+              $('#success').html('<div class="alert alert-success alert-dismissible fade in" role="alert">');
+              $('#success > .alert-success').html('<button type="button" class="close" data-dismiss="alert" aria-label="Close"><span aria-hidden="true">&times;</span></button><p>Your message was <strong>successfully sent</strong>.</p><p>Thank You!</p>').append('</div>');
+              $('#contactForm').trigger("reset");
+            },
+            error: function(){
+              $('#formSubmit').text('hello@studiocraft.cc').prop('disabled', false);
+              $('#fail').html('<div class="alert alert-danger alert-dismissible fade in" role="alert">');
+              $('#fail > .alert-danger').html('<button type="button" class="close" data-dismiss="alert" aria-label="Close"><span aria-hidden="true">&times;</span></button><p>Your message <strong>failed to send</strong>.</p><p>Sorry about that, but please do try again.</p>').append('</div>');
+              $('#contactForm').trigger("reset");
+            }
+          });
         });
 
+      },
+      finalize: function() {
+        // JavaScript to be fired on the home page, after the init JS
         $(document).ready(function() {
           var playlist = [
             {
@@ -67,8 +118,10 @@
               title : "Lakmé - Sous le Dôme Épais",
               author : "Léo Delibes",
             }];
+
           var aud = $('#jukebox audio').get(0);
           aud.pos = -1;
+
           $("a[data-control='play']").click(function() {
             if (aud.pos < 0) {
               $("a[data-control='next']").trigger('click');
@@ -79,6 +132,30 @@
 
           $('a[data-control="pause"]').click(function() {
             aud.pause();
+          });
+
+          $('input[data-control="mute"]').on('change', function() {
+            if( $('#jukebox audio').prop('muted') )
+            {
+              $('#jukebox audio').prop('muted', false);
+              $(this).find("i").toggleClass("fa-volume-off fa-volume-up");
+            } else {
+              $('#jukebox audio').prop('muted', true);
+              $(this).find("i").toggleClass("fa-volume-off fa-volume-up");
+            }
+          });
+
+          $('#jukebox audio').on('play', function() {
+            $('[data-control="volume"]').removeClass('hide').addClass('fade in');
+          });
+
+          $('#jukebox audio').on('pause', function() {
+            $('[data-control="volume"]').addClass('hide').removeClass('fade in');
+          });
+
+          $('input[type="range"]').on('change', function() {
+            var value = $(this).prop("value");
+            $('#jukebox audio').volume = (value / 100);
           });
 
           $('a[data-control="next"]').click(function() {
@@ -97,26 +174,19 @@
             aud.pos--;
             if (aud.pos < 0) aud.pos = playlist.length - 1;
             aud.setAttribute('src', playlist[aud.pos].url);
-
             $('[data-info="title"]').html(playlist[aud.pos].title);
             $('[data-info="author"]').html(playlist[aud.pos].author);
               aud.load();
           });
 
           aud.addEventListener('progress', function() {
-            var width = $(window).width();
-            // var loaded = parseInt(((audio.buffered.end(0) / audio.duration) * 100), 10);
-            var percentLoaded = Math.round(aud.buffered.end(0) / aud.duration * 100);
-            var barWidth = Math.ceil(percentLoaded * (width / 100));
-            $('.load-progress').css('width', barWidth );
-
+            var percentLoaded = Math.round(parseInt(((aud.buffered.end(0) / aud.duration) * 100), 10));
+            $('.load-progress').css('width', percentLoaded + '%');
           });
 
           aud.addEventListener('timeupdate', function() {
-            var width = $(window).width();
-            var percentPlayed = Math.round(aud.currentTime / aud.duration * 1000);
-            var barWidth = Math.ceil(percentPlayed * (width / 1000));
-            $('.play-progress').css('width', barWidth);
+            var percentPlayed = Math.round(aud.currentTime / aud.duration * 100);
+            $('.play-progress').css('width', percentPlayed + '%');
           });
 
           aud.addEventListener('canplay', function() {
@@ -124,16 +194,17 @@
           });
 
           aud.addEventListener('ended', function() {
-            $('adata-control="next"]').trigger('click');
+            $('a[data-control="next"]').trigger('click');
           });
 
           $('[data-info="title"]').html(playlist[0].title);
-          $('[data-info="author"]').html(playlist[0].author);
-          aud.load();
 
+          $('[data-info="author"]').html(playlist[0].author);
+
+          aud.load();
         });
       }
-    }
+    },
   };
 
   // The routing fires all common scripts, followed by the page specific scripts.
--- gulpfile.js
+++ gulpfile.js
@@ -2,26 +2,30 @@
 
 var gulp = require('gulp'),
     sass = require('gulp-ruby-sass'),
-    concat = require('gulp-concat'),
+    scssLint = require('gulp-scss-lint'),
+    sourcemaps = require('gulp-sourcemaps'),
+    cssComb = require('gulp-csscomb'),
     autoprefixer = require('gulp-autoprefixer'),
-    browserSync = require('browser-sync'),
-    rename = require('gulp-rename'),
+    stylestats = require('gulp-stylestats'),
+    minifyCss = require('gulp-minify-css'),
     jshint = require('gulp-jshint'),
     stylish = require('jshint-stylish'),
-    scssLint = require('gulp-scss-lint'),
-    minifyCss = require('gulp-minify-css'),
-    sourcemaps = require('gulp-sourcemaps');
+    concat = require('gulp-concat'),
+    rename = require('gulp-rename'),
+    favicons = require('favicons');
 
+// SASS Task
+//
 
 var styles = (function() {
     return {
         compile: function(config) {
-            gulp.src('assets/styles/' + config.src + '.scss')
+            gulp.src('./assets/styles/' + config.src + '.scss')
                 .pipe(scssLint({
                     'config': 'scss-lint.yml',
                 }));
 
-            return sass('assets/styles/' + config.src + '.scss', {
+            return sass('./assets/styles/' + config.src + '.scss', {
                     precision: 10,
                     sourcemap: true
                 })
@@ -29,46 +33,7 @@ var styles = (function() {
                 .on('error', sass.logError)
                 .pipe(autoprefixer("last 2 version", "> 1%", "ie 8", "ie 7"))
                 .pipe(sourcemaps.write('.'))
-                .pipe(gulp.dest('assets/' + config.dest));
-
-            return this;
-        }
-    };
-}());
-
-var css = (function() {
-    return {
-        compile: function(config) {
-            gulp.src('assets/css/' + config.src + '.css')
-                .pipe(minifyCss({
-                    suffix: '.min'
-                }))
-                .pipe(gulp.dest('assets/' + config.dest));
-
-            return this;
-        }
-    };
-}());
-
-var js = (function() {
-    return {
-        compile: function(config) {
-            gulp.src('assets/scripts/' + config.src + '.js')
-                .pipe(jshint())
-                .pipe(jshint.reporter('jshint-stylish'))
-                .pipe(rename(config.rename + '.js'))
-                .pipe(gulp.dest('assets/' + config.dest));
-
-            return this;
-        }
-    };
-}());
-
-var vendors = (function() {
-    return {
-        compile: function(config) {
-            gulp.src('bower_components/' + config.src)
-                .pipe(gulp.dest('assets/' + config.dest));
+                .pipe(gulp.dest('./assets/' + config.dest));
 
             return this;
         }
@@ -83,13 +48,79 @@ gulp.task('sass', function() {
             });
 });
 
-gulp.task('css', function() {
+// CSS Task
+//
+
+var css = (function() {
+    return {
+        compile: function(config) {
+            gulp.src('./assets/css/' + config.src + '.css')
+                .pipe(cssComb())
+                .pipe(rename(config.rename + '.css'))
+                .pipe(gulp.dest('./assets/' + config.dest))
+                .pipe(minifyCss({
+                    compatibility: 'ie6'
+                  }))
+                .pipe(rename(config.rename + '.min.css'))
+                .pipe(gulp.dest('./assets/' + config.dest));
+
+            return this;
+        }
+    };
+}());
+
+gulp.task('css', ['sass'], function() {
     return css
             .compile({
                 src: 'app',
+                rename: 'main',
                 dest: 'css/dist'
             });
 });
+
+// Style Stats Task
+//
+
+var stats = (function() {
+    return {
+        compile: function(config) {
+          gulp.src('./assets/css/dist/' + config.src + '.css')
+              .pipe(stylestats({
+                  type: config.type,
+                  outfile: true
+            }))
+            .pipe(gulp.dest(config.dest));
+
+            return this;
+        }
+    };
+}());
+
+gulp.task('stylestats', ['css'], function() {
+    return stats
+        .compile({
+            src: 'main',
+            type: 'json',
+            dest: 'stats'
+        });
+});
+
+// JS Task
+//
+
+var js = (function() {
+    return {
+        compile: function(config) {
+            gulp.src('./assets/scripts/' + config.src + '.js')
+                .pipe(jshint())
+                .pipe(jshint.reporter('jshint-stylish'))
+                .pipe(rename(config.rename + '.js'))
+                .pipe(gulp.dest('./assets/' + config.dest));
+
+            return this;
+        }
+    };
+}());
 
 gulp.task('js', function() {
     return js
@@ -100,6 +131,113 @@ gulp.task('js', function() {
             });
 });
 
+// Bower Components Task
+//
+
+// var wiredep = require('wiredep').stream;
+//
+//
+// var bower = (function() {
+//     return {
+//         compile: function(config) {
+//             gulp.src('./assets/scripts/' + config.src + '.js')
+//                 .pipe(jshint())
+//                 .pipe(jshint.reporter('jshint-stylish'))
+//                 .pipe(rename(config.rename + '.js'))
+//                 .pipe(gulp.dest('./assets/' + config.dest));
+//
+//             return this;
+//         }
+//     };
+// }());
+//
+// gulp.task('wiredep', function() {
+//     return bower
+//             .compile({
+//                 src: 'jquery/dist/*.js',
+//                 dest: 'js/vendors'
+//             })
+//             .compile({
+//                 src: 'respond/dest/*.js',
+//                 dest: 'js/vendors'
+//             })
+//             .compile({
+//                 src: 'app',
+//                 rename: 'main',
+//                 dest: 'js'
+//             });
+// });
+//
+// gulp.task('bower', function () {
+//   gulp.src('./src/footer.html')
+//     .pipe(wiredep({
+//       optional: 'configuration',
+//       goes: 'here'
+//     }))
+//     .pipe(gulp.dest('./dest'));
+// });
+
+// Favicons Task
+//
+
+var icons = (function() {
+    return {
+        compile: function(config) {
+            return favicons({
+                files: {
+                    src: './assets/' + config.src + 'favicon.src.png',
+                    dest: './assets/' + config.dest,
+                    html: config.html,
+                    iconsPath: './assets/' + config.path,
+                },
+                icons: {
+                    android: true,
+                    appleIcon: true,
+                    appleStartup: true,
+                    coast: true,
+                    favicons: true,
+                    firefox: true,
+                    opengraph: true,
+                    windows: true,
+                    yandex: true
+                },
+                  settings: {
+                      background: config.bg,
+                      logging: true
+                  },
+                favicon_generation: null,
+            });
+
+            return this;
+        }
+    };
+}());
+
+gulp.task('icons', function() {
+    return icons
+        .compile({
+            src: 'images/favicons/',
+            dest: 'images/favicons/',
+            html: '_includes/favicons.html',
+            path: 'images/favicons/',
+            bg: 'transparent'
+        });
+});
+
+// TODO: Gulp Bower Injecting for Vendor Files
+var vendors = (function() {
+    return {
+        compile: function(config) {
+            gulp.src('bower_components/' + config.src)
+                .pipe(gulp.dest('assets/' + config.dest));
+
+            return this;
+        }
+    };
+}());
+
+
+// TODO: Gulp Bower Injecting for Vendor Files
 gulp.task('vendorScripts', function() {
     return vendors
             .compile({
@@ -115,11 +253,11 @@ gulp.task('vendorScripts', function() {
                 dest: 'js/vendors'
             })
             .compile({
-                src: 'masonry/dist/*.js',
+                src: 'headroom.js/dist/*.js',
                 dest: 'js/vendors'
             })
             .compile({
-                src: 'imagesloaded/*.js',
+                src: 'wow/dist/*.js',
                 dest: 'js/vendors'
             });
 });
@@ -138,12 +276,17 @@ gulp.task('vendorFonts', function() {
 
 gulp.task('vendorStyles', function() {
     return vendors
+        .compile({
+            src: 'animate.css/*.css',
+            dest: 'css/vendors'
+        });
 });
 
 gulp.task('watch', function() {
-    gulp.watch(['assets/styles/**/*.scss'], ['sass', 'css']);
+    gulp.watch(['assets/styles/**/*.scss'], ['css']);
     gulp.watch(['assets/scripts/**/*.js'], ['js']);
+    gulp.watch(['assets/images/favicons/*.src.png'], ['icons']);
 });
 
 gulp.task('vendors', ['vendorScripts', 'vendorStyles', 'vendorFonts']);
-gulp.task('default', ['sass', 'css', 'js', 'watch', 'vendors']);
+gulp.task('default', ['stylestats', 'js', 'icons', 'watch']);
