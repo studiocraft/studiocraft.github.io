2cbafed727f1bfd6dbb5619ed41da68960dcd640
Bryan Colosky <bcolosky@gmail.com>
2016-03-01 22:34:45 -0500

    redesign

--- gulpfile.js
+++ gulpfile.js
@@ -7,12 +7,13 @@ var gulp = require('gulp'),
     cssComb = require('gulp-csscomb'),
     autoprefixer = require('gulp-autoprefixer'),
     stylestats = require('gulp-stylestats'),
-    minifyCss = require('gulp-minify-css'),
+    cssnano = require('gulp-cssnano'),
     jshint = require('gulp-jshint'),
     stylish = require('jshint-stylish'),
     concat = require('gulp-concat'),
+    uglify = require('gulp-uglify'),
     rename = require('gulp-rename'),
-    favicons = require('favicons');
+    favicons = require("gulp-favicons");
 
 // SASS Task
 //
@@ -33,6 +34,8 @@ var styles = (function() {
                 .on('error', sass.logError)
                 .pipe(autoprefixer("last 2 version", "> 1%", "ie 8", "ie 7"))
                 .pipe(sourcemaps.write('.'))
+                .pipe(gulp.dest('./assets/' + config.dest))
+                .pipe(rename(config.rename + '.css'))
                 .pipe(gulp.dest('./assets/' + config.dest));
 
             return this;
@@ -42,10 +45,11 @@ var styles = (function() {
 
 gulp.task('sass', function() {
     return styles
-            .compile({
-                src: 'app',
-                dest: 'css'
-            });
+        .compile({
+            src: 'app',
+            rename: 'main',
+            dest: 'css'
+        });
 });
 
 // CSS Task
@@ -56,13 +60,12 @@ var css = (function() {
         compile: function(config) {
             gulp.src('./assets/css/' + config.src + '.css')
                 .pipe(cssComb())
-                .pipe(rename(config.rename + '.css'))
                 .pipe(gulp.dest('./assets/' + config.dest))
-                .pipe(minifyCss({
+                .pipe(cssnano({
                     compatibility: 'ie6'
                   }))
                 .pipe(rename(config.rename + '.min.css'))
-                .pipe(gulp.dest('./assets/' + config.dest));
+                .pipe(gulp.dest('./dist/' + config.dest));
 
             return this;
         }
@@ -71,11 +74,11 @@ var css = (function() {
 
 gulp.task('css', ['sass'], function() {
     return css
-            .compile({
-                src: 'app',
-                rename: 'main',
-                dest: 'css/dist'
-            });
+        .compile({
+            src: 'main',
+            rename: 'main',
+            dest: 'css/'
+        });
 });
 
 // Style Stats Task
@@ -86,7 +89,7 @@ var stats = (function() {
         compile: function(config) {
           gulp.src('./assets/css/dist/' + config.src + '.css')
               .pipe(stylestats({
-                  type: config.type,
+                  type: config.format,
                   outfile: true
             }))
             .pipe(gulp.dest(config.dest));
@@ -96,19 +99,19 @@ var stats = (function() {
     };
 }());
 
-gulp.task('stylestats', ['css'], function() {
+gulp.task('stats', ['css'], function() {
     return stats
         .compile({
             src: 'main',
-            type: 'json',
-            dest: 'stats'
+            format: 'json',
+            dest: 'assets/stats'
         });
 });
 
-// JS Task
+// Scripts Task
 //
 
-var js = (function() {
+var scripts = (function() {
     return {
         compile: function(config) {
             gulp.src('./assets/scripts/' + config.src + '.js')
@@ -122,60 +125,39 @@ var js = (function() {
     };
 }());
 
-gulp.task('js', function() {
-    return js
-            .compile({
-                src: 'app',
-                rename: 'main',
-                dest: 'js'
-            });
+gulp.task('scripts', function() {
+    return scripts
+        .compile({
+            src: 'app',
+            rename: 'main',
+            dest: 'js'
+        });
 });
 
-// Bower Components Task
+// JS Task
 //
 
-// var wiredep = require('wiredep').stream;
-//
-//
-// var bower = (function() {
-//     return {
-//         compile: function(config) {
-//             gulp.src('./assets/scripts/' + config.src + '.js')
-//                 .pipe(jshint())
-//                 .pipe(jshint.reporter('jshint-stylish'))
-//                 .pipe(rename(config.rename + '.js'))
-//                 .pipe(gulp.dest('./assets/' + config.dest));
-//
-//             return this;
-//         }
-//     };
-// }());
-//
-// gulp.task('wiredep', function() {
-//     return bower
-//             .compile({
-//                 src: 'jquery/dist/*.js',
-//                 dest: 'js/vendors'
-//             })
-//             .compile({
-//                 src: 'respond/dest/*.js',
-//                 dest: 'js/vendors'
-//             })
-//             .compile({
-//                 src: 'app',
-//                 rename: 'main',
-//                 dest: 'js'
-//             });
-// });
-//
-// gulp.task('bower', function () {
-//   gulp.src('./src/footer.html')
-//     .pipe(wiredep({
-//       optional: 'configuration',
-//       goes: 'here'
-//     }))
-//     .pipe(gulp.dest('./dest'));
-// });
+var js = (function() {
+    return {
+        compile: function(config) {
+            gulp.src('./assets/js/' + config.src + '.js')
+                .pipe(uglify())
+                .pipe(rename(config.rename + 'min.js'))
+                .pipe(gulp.dest('./dist/' + config.dest));
+
+            return this;
+        }
+    };
+}());
+
+gulp.task('js', ['scripts'], function() {
+    return js
+        .compile({
+            src: 'main',
+            rename: 'main',
+            dest: 'js'
+        });
+});
 
 // Favicons Task
 //
@@ -183,30 +165,24 @@ gulp.task('js', function() {
 var icons = (function() {
     return {
         compile: function(config) {
-            return favicons({
-                files: {
-                    src: './assets/' + config.src + 'favicon.src.png',
-                    dest: './assets/' + config.dest,
-                    html: config.html,
-                    iconsPath: './assets/' + config.path,
-                },
-                icons: {
-                    android: true,
-                    appleIcon: true,
-                    appleStartup: true,
-                    coast: true,
-                    favicons: true,
-                    firefox: true,
-                    opengraph: true,
-                    windows: true,
-                    yandex: true
-                },
-                  settings: {
-                      background: config.bg,
-                      logging: true
-                  },
-                favicon_generation: null,
-            });
+          gulp.src('assets/' + config.src + 'favicon.src.png')
+              .pipe(favicons({
+                  appName: "My Favicons",
+                  appDescription: "These are my Favicons",
+                  developerName: "Bryan Colosky",
+                  developerURL: "http://bryancolosky.com/",
+                  background: config.background,
+                  path: 'assets/' + config.path,
+                  url: "http://bryancolosky.com/",
+                  display: "standalone",
+                  orientation: "landscape",
+                  version: 1.0,
+                  logging: false,
+                  online: false,
+                  html: config.html,
+                  replace: true
+              }))
+              .pipe(gulp.dest('assets/' + config.dest));
 
             return this;
         }
@@ -220,73 +196,15 @@ gulp.task('icons', function() {
             dest: 'images/favicons/',
             html: '_includes/favicons.html',
             path: 'images/favicons/',
-            bg: 'transparent'
+            background: 'transparent'
         });
-});
 
-// TODO: Gulp Bower Injecting for Vendor Files
-var vendors = (function() {
-    return {
-        compile: function(config) {
-            gulp.src('bower_components/' + config.src)
-                .pipe(gulp.dest('assets/' + config.dest));
-
-            return this;
-        }
-    };
-}());
-
-
-// TODO: Gulp Bower Injecting for Vendor Files
-gulp.task('vendorScripts', function() {
-    return vendors
-            .compile({
-                src: 'jquery/dist/*.js',
-                dest: 'js/vendors'
-            })
-            .compile({
-                src: 'respond/dest/*.js',
-                dest: 'js/vendors'
-            })
-            .compile({
-                src: 'bootstrap-sass/assets/javascripts/*.js',
-                dest: 'js/vendors'
-            })
-            .compile({
-                src: 'headroom.js/dist/*.js',
-                dest: 'js/vendors'
-            })
-            .compile({
-                src: 'wow/dist/*.js',
-                dest: 'js/vendors'
-            });
-});
-
-gulp.task('vendorFonts', function() {
-    return vendors
-            .compile({
-                src: 'bootstrap-sass/assets/fonts/bootstrap/*',
-                dest: 'fonts/vendors'
-            })
-            .compile({
-                src: 'font-awesome/fonts/*',
-                dest: 'fonts/vendors'
-            });
-});
-
-gulp.task('vendorStyles', function() {
-    return vendors
-        .compile({
-            src: 'animate.css/*.css',
-            dest: 'css/vendors'
-        });
 });
 
 gulp.task('watch', function() {
-    gulp.watch(['assets/styles/**/*.scss'], ['css']);
-    gulp.watch(['assets/scripts/**/*.js'], ['js']);
+    gulp.watch(['assets/styles/**/*.scss'], ['sass']);
+    gulp.watch(['assets/scripts/**/*.js'], ['scripts']);
     gulp.watch(['assets/images/favicons/*.src.png'], ['icons']);
 });
 
-gulp.task('vendors', ['vendorScripts', 'vendorStyles', 'vendorFonts']);
-gulp.task('default', ['stylestats', 'js', 'icons', 'watch']);
+gulp.task('default', ['css', 'js', 'watch']);
